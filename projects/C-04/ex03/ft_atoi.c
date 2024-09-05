dc682c5cb1eaf711e98f7b5002184e0b3b99e6c0
gangel-a <gangel-a@student.42sp.org.br>
2024-09-05 18:11:31 -0300

    adds projects to folders

---
+++ projects/C-04/ex03/ft_atoi.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gangel-a <gangel-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 12:31:44 by gangel-a          #+#    #+#             */
+/*   Updated: 2024/09/03 17:41:52 by gangel-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str);
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	nb;
+	int	sign;
+
+	i = 0;
+	nb = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb *= 10;
+		nb += (str[i] - '0');
+		i++;
+	}
+	nb *= sign;
+	return (nb);
+}
+
+/*
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		printf("Number: %i\n", ft_atoi(argv[1]));
+	}
+	return (0);
+}
+*/
