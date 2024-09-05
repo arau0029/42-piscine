a9023ade1478816d2823cb2668976595b3df8d6e
gangel-a <gangel-a@student.42sp.org.br>
2024-09-05 18:06:15 -0300

    adds projects to folders

---
+++ projects/C-06/ex00/ft_print_program_name.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gangel-a <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 19:04:23 by gangel-a          #+#    #+#             */
+/*   Updated: 2024/09/04 19:10:49 by gangel-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_program_name(char *name)
+{
+	int	i;
+
+	i = 0;
+	while (name[i])
+	{
+		write(1, &name[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		ft_print_program_name(argv[0]);
+	}
+}
